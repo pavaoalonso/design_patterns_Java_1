2788c538a0f7f775eab8ae65e0c7854e47e10285
Paulo Alonso <alopaulohenrique@gmail.com>
2021-01-21 23:25:46 -0300

    foi

--- workspace/src/br/com/alura/loja/TestesImpostos.java
+++ workspace/src/br/com/alura/loja/TestesImpostos.java
@@ -3,7 +3,8 @@ package br.com.alura.loja;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.imposto.CalculadorDeImpostos;
-import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
 
 public class TestesImpostos {
@@ -11,9 +12,11 @@ public class TestesImpostos {
 	public static void main(String[] args) {
 
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
-		CalculadorDeImpostos calculadora = new CalculadorDeImpostos();
+		
+		System.out.println(CalculadorDeImpostos.calcular(orcamento, new ICMS()));
+		
+		System.out.println(CalculadorDeImpostos.calcular(orcamento, new ISS()));
 
-		System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
 	}
 
 }
